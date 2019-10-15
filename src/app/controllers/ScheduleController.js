f0415f8e4d3e5866709940ef9d01123f2109cbb2
Mayke <make_zs@hotmail.com>
2019-10-14 21:37:42 -0300

    Ajustes para o front

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -1,18 +1,15 @@
 import Appointment from '../models/Appointment';
 import User from '../models/User';
-import * as Yup from 'yup';
 import { startOfDay, endOfDay, parseISO } from 'date-fns';
-import File from '../models/File';
 import { Op } from 'sequelize';
-import { start } from 'repl';
 
 class ScheduleController {
     async index(req, res) {
-        const chechUserProvider = await User.findOne({
+        const checkUserProvider = await User.findOne({
             where: { id: req.userId, provider: true }
         });
 
-        if (!chechUserProvider) {
+        if (!checkUserProvider) {
             return res.status(401).json({ error: 'User must be a provider' });
         }
 
@@ -26,7 +23,14 @@ class ScheduleController {
                 date: {
                     [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)]
                 }
-            }
+            },
+            include: [
+                {
+                    model: User,
+                    as: 'user',
+                    attributes: ['name']
+                }
+            ]
         });
 
         return res.json(appointments);
